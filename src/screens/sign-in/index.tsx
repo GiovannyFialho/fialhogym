d710aa0ea73ee8ff2cbde90ec41966490161dca1
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-04-08 21:50:13 -0300

    finalizando sign-in

--- src/screens/sign-in/index.tsx
+++ src/screens/sign-in/index.tsx
@@ -1,3 +1,11 @@
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TouchableWithoutFeedback,
+} from "react-native";
+
 import { Box } from "@/components/ui/box";
 import { Heading } from "@/components/ui/heading";
 import { Image } from "@/components/ui/image";
@@ -12,52 +20,67 @@ import Logo from "@/assets/logo.svg";
 
 export function SignIn() {
   return (
-    <VStack>
-      <Image
-        className="absolute h-[624px] w-full"
-        source={BackgroundImg}
-        alt="Pessoas treinando"
-        defaultSource={BackgroundImg}
-      />
+    <KeyboardAvoidingView
+      className="flex-1"
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          showsVerticalScrollIndicator={false}
+          keyboardShouldPersistTaps="handled"
+        >
+          <VStack>
+            <Image
+              className="absolute h-[624px] w-full"
+              source={BackgroundImg}
+              alt="Pessoas treinando"
+              defaultSource={BackgroundImg}
+            />
 
-      <VStack className="px-10 pb-16">
-        <Box className="my-24 items-center justify-center">
-          <Logo />
+            <VStack className="px-10 pb-16">
+              <Box className="my-24 items-center justify-center">
+                <Logo />
 
-          <Text className="text-sm color-slate-100">
-            Treine sua mente e o seu corpo.
-          </Text>
-        </Box>
+                <Text className="text-sm color-slate-100">
+                  Treine sua mente e o seu corpo.
+                </Text>
+              </Box>
 
-        <Box className="my-24 items-center justify-center gap-2">
-          <Heading className="text-2xl text-gray-100">Acesse a conta</Heading>
+              <Box className="my-24 items-center justify-center gap-2">
+                <Heading className="text-2xl text-gray-100">
+                  Acesse a conta
+                </Heading>
 
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
+                <Input
+                  placeholder="E-mail"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                />
 
-          <Input placeholder="Senha" secureTextEntry />
+                <Input placeholder="Senha" secureTextEntry />
 
-          <Button
-            title="Acessar"
-            className="h-14 w-full rounded-sm border-green-300 bg-green-500"
-          />
-        </Box>
+                <Button
+                  title="Acessar"
+                  className="h-14 w-full rounded-sm border-green-300 bg-green-500"
+                />
+              </Box>
 
-        <Box className="mt-4 items-center justify-end">
-          <Text className="mb-3 text-sm font-medium text-gray-100">
-            Ainda não tem acesso?
-          </Text>
+              <Box className="mt-4 items-center justify-end">
+                <Text className="mb-3 text-sm font-medium text-gray-100">
+                  Ainda não tem acesso?
+                </Text>
 
-          <Button
-            title="Criar conta"
-            variant="outline"
-            className="h-14 w-full rounded-sm border-green-300"
-          />
-        </Box>
-      </VStack>
-    </VStack>
+                <Button
+                  title="Criar conta"
+                  variant="outline"
+                  className="h-14 w-full rounded-sm border-green-300"
+                />
+              </Box>
+            </VStack>
+          </VStack>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
