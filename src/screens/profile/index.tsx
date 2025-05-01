e4790fa47db0790d07af6b64a73c65edcc251782
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-05-01 10:15:55 -0300

    criating header in User page

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,10 +1,25 @@
+import { ScrollView } from "react-native";
+
 import { Box } from "@/components/ui/box";
-import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
+
+import { ScreenHeader } from "@/components/screen-header";
+import { UserPhoto } from "@/components/user-photo";
 
 export function Profile() {
   return (
-    <Box className="items-center justify-center">
-      <Text className="mt-10 text-3xl text-white">Perfil</Text>
-    </Box>
+    <VStack className="flex-1">
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Box className="mt-6 items-center justify-center px-10">
+          <UserPhoto
+            source={{ uri: "https://github.com/GiovannyFialho.png" }}
+            alt="Foto do usuário"
+            size="lg"
+          />
+        </Box>
+      </ScrollView>
+    </VStack>
   );
 }
