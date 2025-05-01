c3370a82379ff443757a4989c9fcd26a653d3e92
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-05-01 11:44:43 -0300

    adjusting exercice page header

--- src/screens/exercise/index.tsx
+++ src/screens/exercise/index.tsx
@@ -4,8 +4,13 @@ import { TouchableOpacity } from "react-native";
 
 import { type NavigationProps } from "@/routes/app.routes";
 
+import { Heading } from "@/components/ui/heading";
+import { HStack } from "@/components/ui/hstack";
 import { Icon } from "@/components/ui/icon";
+import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
+
+import BodySvg from "@/assets/body.svg";
 
 export function Exercise() {
   const navigation = useNavigation<NavigationProps>();
@@ -16,10 +21,22 @@ export function Exercise() {
 
   return (
     <VStack className="flex-1">
-      <VStack className="bg-darkGray px-8 pb-12 pt-24">
+      <VStack className="bg-darkGray px-8 pt-14">
         <TouchableOpacity onPress={handleGoBack}>
           <Icon as={ArrowLeft} className="color-green-500" size="xl" />
         </TouchableOpacity>
+
+        <HStack className="mt-4 items-center justify-between pb-8">
+          <Heading className="shrink text-lg font-bold text-gray-100">
+            Puxada frontal
+          </Heading>
+
+          <HStack className="items-center">
+            <BodySvg />
+
+            <Text className="ml-1 capitalize text-gray-200">Costas</Text>
+          </HStack>
+        </HStack>
       </VStack>
     </VStack>
   );
