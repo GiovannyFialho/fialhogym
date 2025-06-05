fe9ea1942cc49e8556dd4af72e7aaa0ecb7ee20d
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-06-05 19:57:41 -0300

    adjusting update profile form

--- mobile/src/screens/profile/index.tsx
+++ mobile/src/screens/profile/index.tsx
@@ -1,7 +1,11 @@
 import * as FileSystem from "expo-file-system";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { ScrollView, TouchableOpacity } from "react-native";
+import { z } from "zod";
+
+import { useAuth } from "@/hooks/useAuth";
 
 import { Box } from "@/components/ui/box";
 import { Heading } from "@/components/ui/heading";
@@ -14,13 +18,79 @@ import { Input } from "@/components/input";
 import { ScreenHeader } from "@/components/screen-header";
 import { ToastMessage } from "@/components/toast-message";
 import { UserPhoto } from "@/components/user-photo";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formDataSchema = z
+  .object({
+    name: z.string().min(1, "Informe o nome"),
+    email: z.string(),
+    old_password: z
+      .string()
+      .transform((value) => (value === "" ? null : value))
+      .nullable()
+      .optional(),
+    password: z
+      .string()
+      .transform((value) => (value === "" ? null : value))
+      .nullable()
+      .optional(),
+    password_confirm: z
+      .string()
+      .transform((value) => (value === "" ? null : value))
+      .nullable()
+      .optional(),
+  })
+  .superRefine((data, ctx) => {
+    if (data.password && data.password.length < 6) {
+      ctx.addIssue({
+        path: ["password"],
+        message: "A senha deve ter pelo menos 6 caracteres",
+        code: z.ZodIssueCode.custom,
+      });
+    }
+
+    if (data.password_confirm && data.password_confirm.length < 6) {
+      ctx.addIssue({
+        path: ["password_confirm"],
+        message: "Confirme sua senha",
+        code: z.ZodIssueCode.custom,
+      });
+    }
+
+    if (data.password || data.password_confirm) {
+      if (data.password !== data.password_confirm) {
+        ctx.addIssue({
+          path: ["password_confirm"],
+          message: "As senhas não coincidem",
+          code: z.ZodIssueCode.custom,
+        });
+      }
+    }
+  });
+
+type FormDataProps = z.infer<typeof formDataSchema>;
 
 export function Profile() {
   const [userPhoto, setUserPhoto] = useState(
     "https://github.com/GiovannyFialho.png",
   );
 
+  const { user } = useAuth();
   const toast = useToast();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    resolver: zodResolver(formDataSchema),
+    defaultValues: {
+      name: user.name,
+      email: user.email,
+      old_password: "",
+      password: "",
+      password_confirm: "",
+    },
+  });
 
   async function handleUserPhotoSelect() {
     try {
@@ -63,6 +133,10 @@ export function Profile() {
     }
   }
 
+  async function handleProfileUpdate(data: FormDataProps) {
+    console.log({ data });
+  }
+
   return (
     <VStack className="flex-1">
       <ScreenHeader title="Perfil" />
@@ -82,12 +156,32 @@ export function Profile() {
           </TouchableOpacity>
 
           <Box className="w-full gap-4">
-            <Input placeholder="Nome" className="bg-darkGray" />
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { value, onChange } }) => (
+                <Input
+                  value={value}
+                  placeholder="Nome"
+                  className="bg-darkGray text-white"
+                  onChangeText={onChange}
+                  errorMessage={errors.name?.message}
+                />
+              )}
+            />
 
-            <Input
-              value="giovanny@email.com"
-              className="bg-darkGray"
-              isReadOnly
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { value, onChange } }) => (
+                <Input
+                  value={value}
+                  isReadOnly
+                  placeholder="E-mail"
+                  className="bg-darkGray text-white"
+                  onChangeText={onChange}
+                />
+              )}
             />
           </Box>
 
@@ -96,25 +190,54 @@ export function Profile() {
           </Heading>
 
           <Box className="w-full gap-4">
-            <Input
-              secureTextEntry
-              placeholder="Senha antiga"
-              className="bg-darkGray"
+            <Controller
+              control={control}
+              name="old_password"
+              render={({ field: { value, onChange } }) => (
+                <Input
+                  value={value}
+                  secureTextEntry
+                  placeholder="Senha antiga"
+                  className="bg-darkGray text-white"
+                  onChangeText={onChange}
+                />
+              )}
             />
 
-            <Input
-              secureTextEntry
-              placeholder="Nova senha"
-              className="bg-darkGray"
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { value, onChange } }) => (
+                <Input
+                  value={value}
+                  secureTextEntry
+                  placeholder="Nova senha"
+                  className="bg-darkGray text-white"
+                  onChangeText={onChange}
+                  errorMessage={errors.password?.message}
+                />
+              )}
             />
 
-            <Input
-              secureTextEntry
-              placeholder="Confirme a nova senha"
-              className="bg-darkGray"
+            <Controller
+              control={control}
+              name="password_confirm"
+              render={({ field: { value, onChange } }) => (
+                <Input
+                  value={value}
+                  secureTextEntry
+                  placeholder="Confirme a nova senha"
+                  className="bg-darkGray text-white"
+                  onChangeText={onChange}
+                  errorMessage={errors.password_confirm?.message}
+                />
+              )}
             />
 
-            <Button title="Atualizar" />
+            <Button
+              title="Atualizar"
+              onPress={handleSubmit(handleProfileUpdate)}
+            />
           </Box>
         </Box>
       </ScrollView>
